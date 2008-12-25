d3fa91a5baec3728b50bb29769646fe407e4637c
hyperfinder <hyperfinder@f0c0bd8c-f357-455b-8b8b-52c4c1bcb045>
2008-12-25 21:06:37 +0000

    changed "$userid" to "$userid_" (Name könnte sich ev. mit Cookie-Namen überschneiden...)

--- hyperfinder07/web/cmd/_user_cookies.php
+++ hyperfinder07/web/cmd/_user_cookies.php
@@ -8,7 +8,7 @@ $userpwd_ = $_COOKIE["userpwd"];
 // Falls userid und userpwd nicht zusammenpassen wird eine neue userid generiert.
 // Grund: Es kann aus nicht geklärten Gründen vorkommen, dass userid und userpwd nicht zusammenpassen.
 //        Falls in diesem Fall einfach eine neue userid zugewiesen wird, lässt sich der account aber nicht hacken.
-if (!isUserPwdMismatch($userid_, $userpwd_) || !existsUser($userid_) || $userid_ == null || $userid_ == "" || $userpwd_ == null || $userpwd_ == "" ) {
+if (isUserPwdMismatch($userid_, $userpwd_) || !existsUser($userid_) || $userid_ == null || $userid_ == "" || $userpwd_ == null || $userpwd_ == "" ) {
 	$userid_ = getNextId();
 	$userpwd_ = createCode(64);
 	storeUserPwd($userid_, $userpwd_);
